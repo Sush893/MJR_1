74961d1ec49261963c480df7cceb5cfa7adf4ca9
Sush893 <suhani12345singh@gmail.com>
2025-03-27 00:28:16 +0530

    commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -13,7 +13,7 @@ export const authMiddleware = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
-    next();
+    next(e);
   } catch (error) {
     res.status(403).json({ message: "Invalid Token" });
   }
